601e35344884e56d1c2f5c2cf17bc3d8ac1413c3
astorlucas <lucasastor@gmail.com>
2022-04-11 13:05:58 -0300

    spring example
    prototype spring

---
+++ delidrones/src/main/java/com/ssoo/delidrones/DelidronesApplication.java
@@ -0,0 +1,13 @@
+package com.ssoo.delidrones;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DelidronesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DelidronesApplication.class, args);
+	}
+
+}
