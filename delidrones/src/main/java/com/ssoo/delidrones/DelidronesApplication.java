f55187db8a0a1b2cdb12890dfe8ab5ab5cffd411
astorlucas <lucasastor@gmail.com>
2022-04-19 13:42:12 -0300

    data created from csv
    data not passed by http request, but it is called inside the aplication by a new thread

--- delidrones/src/main/java/com/ssoo/delidrones/DelidronesApplication.java
+++ delidrones/src/main/java/com/ssoo/delidrones/DelidronesApplication.java
@@ -1,5 +1,6 @@
 package com.ssoo.delidrones;
 
+import com.ssoo.delidrones.datos.ClienteDato;
 import com.ssoo.delidrones.negocio.Cliente;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -8,6 +9,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DelidronesApplication {
 
 	public static void main(String[] args) {
+		ClienteDato cli = new ClienteDato();
+		Thread hilo = new Thread(cli,"hilo Update");
+		hilo.start();
+		
 		SpringApplication.run(DelidronesApplication.class, args);
 
 	}
